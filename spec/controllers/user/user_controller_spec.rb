c95c59db869ddd5524ed0335499860e5bd38cea3
woffles <wolfgang.truong@gmail.com>
2017-11-12 08:46:53 -0800

    fixed some issues with rspec

--- spec/controllers/user/user_controller_spec.rb
+++ spec/controllers/user/user_controller_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe WelcomeController, type: :controller do
+RSpec.describe UserController, type: :controller do
+  before(:each) do
+    @my_user = FactoryGirl.create(:user)
+    sign_in @my_user
+  end
 
+  describe "GET #index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
